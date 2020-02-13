da07f250d418a8fb67a4802615eaff397fa9ad3a
Huangyicai <yicai.huang@thoughtworks.com>
2020-02-13 14:47:15 +0800

    [Chore]:add spring boot base and main class.

---
+++ src/main/java/com/thoughtworks/leanengine/Greeting.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.leanengine;
+
+import lombok.Getter;
+
+@Getter
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/thoughtworks/leanengine/LeanEngineApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.leanengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeanEngineApplication.class, args);
+    }
+}
