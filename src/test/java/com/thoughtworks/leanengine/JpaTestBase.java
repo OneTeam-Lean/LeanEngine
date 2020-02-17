1ad6c3b5e62dc43fbf97ff38849a6b5a2addb985
Huangyicai <yicai.huang@thoughtworks.com>
2020-02-17 15:57:21 +0800

    [Chore]:use junit5 to test and update JpaTestBase implements.

--- src/test/java/com/thoughtworks/leanengine/JpaTestBase.java
+++ src/test/java/com/thoughtworks/leanengine/JpaTestBase.java
@@ -1,15 +1,12 @@
 package com.thoughtworks.leanengine;
 
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class JpaTestBase {
-  @Autowired private TestEntityManager entdityManager;
+  @Autowired private TestEntityManager entityManager;
 }
--- src/test/java/com/thoughtworks/leanengine/infra/repo/PersonRepositoryJpaTest.java
+++ src/test/java/com/thoughtworks/leanengine/infra/repo/PersonRepositoryJpaTest.java
@@ -3,13 +3,15 @@ package com.thoughtworks.leanengine.infra.repo;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.thoughtworks.leanengine.JpaTestBase;
 import com.thoughtworks.leanengine.infra.repo.po.person.PersonPO;
 import com.thoughtworks.leanengine.personcontext.Person.Person;
 import java.util.List;
-import org.junit.Test;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class PersonRepositoryJpaTest extends JpaTestBase {
@@ -27,6 +29,12 @@ public class PersonRepositoryJpaTest extends JpaTestBase {
   }
 
   @Test
+  public void return_3_size_when_count_person() {
+    long allCnt = personRepo.count();
+    assertThat(allCnt, is(3L));
+  }
+
+  @Test
   public void return_1_when_insert_person() {
     Person person = new Person("test1", "note1");
     long originCnt = personRepo.count();
@@ -39,5 +47,28 @@ public class PersonRepositoryJpaTest extends JpaTestBase {
   public void return_3_persons_when_find_all() {
     List<PersonPO> all = (List<PersonPO>) personRepo.findAll();
     assertThat(all.size(), is(3));
+  }
+
+  @Test
+  public void return_2_persons_when_delete_one() {
+    List<PersonPO> personPOS = (List<PersonPO>) personRepo.findAll();
+    Long needDeleteId = personPOS.get(1).getId();
+    personRepo.deleteById(needDeleteId);
+    List<PersonPO> deletedPersons = (List<PersonPO>) personRepo.findAll();
+    assertThat(deletedPersons.size(), is(2));
+  }
+
+  @Test
+  public void return_name_is_changed_when_save_person_change_name() {
+    List<PersonPO> personPOS = (List<PersonPO>) personRepo.findAll();
+    PersonPO originPerson = personPOS.get(1);
+    PersonPO changedPerson = new PersonPO(originPerson.getId(), "changed", originPerson.getNote());
+    personRepo.save(changedPerson);
+    Optional<PersonPO> personById = personRepo.findById(changedPerson.getId());
+    assertTrue(personById.isPresent());
+    PersonPO personPO = personById.get();
+    assertThat(personPO.getName(), is("changed"));
+    assertThat(personPO.getNote(), is("note1"));
+    assertThat(personRepo.count(), is(3L));
   }
 }
