a68f4d7996f93693578e87471ec8e0c121a3682b
idolice <349031587@qq.com>
2020-02-20 13:34:14 +0800

    [Feat]: add swagger

---
+++ src/test/java/com/thoughtworks/leanengine/configure/SpringFoxConfigTest.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.leanengine.configure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SpringFoxConfigTest {
+  @Autowired MockMvc mockMvc;
+
+  @Test
+  public void shouldEnableSwagger() throws Exception {
+    mockMvc.perform(get("/swagger-ui.html")).andExpect(status().is(200));
+  }
+}
