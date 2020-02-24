4cd793fb1b9f23bc61875a9e0993f0e6eab7d3f9
idolice <349031587@qq.com>
2020-02-24 17:27:53 +0800

    [Feat]: add data annotation

--- src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/tasks/AutoTask.java
+++ src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/tasks/AutoTask.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.leanengine.domain.workflowcontext.tasks;
 
 import com.thoughtworks.leanengine.domain.workflowcontext.interfaces.Activity;
+import lombok.Data;
 
+@Data
 public class AutoTask extends Activity {}
--- src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/tasks/ManualTask.java
+++ src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/tasks/ManualTask.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.leanengine.domain.workflowcontext.tasks;
 
 import com.thoughtworks.leanengine.domain.workflowcontext.interfaces.Activity;
+import lombok.Data;
 
+@Data
 public class ManualTask extends Activity {
   private String triggeredBy;
 }
