4872a8228e37f01aa5533ddf3fe76f2a217f6798
Huangyicai <yicai.huang@thoughtworks.com>
2020-03-04 14:52:12 +0800

    [Chore]: add CORS configuration to resolve cross-origin problem.

---
+++ src/main/java/com/thoughtworks/leanengine/infra/conf/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.leanengine.infra.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/workflow")
+            .allowedOrigins(
+                "http://http://ec2-161-189-97-207.cn-northwest-1.compute.amazonaws.com.cn:9003");
+      }
+    };
+  }
+}
