e1daf43e99d193fa50eb9b14d5b51a6072ef7cd5
idolice <349031587@qq.com>
2020-03-04 13:20:09 +0800

    [Feat]: refactor

--- src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/interfaces/Gateway.java
+++ src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/interfaces/Gateway.java
@@ -5,6 +5,7 @@ import com.thoughtworks.leanengine.domain.workflowcontext.enums.ComponentType;
 public abstract class Gateway extends Component {
   private String firstFlowId;
   private String secondFlowId;
+  private Boolean isStartGateway;
 
   public Gateway(ComponentType componentType) {
     super(componentType);
