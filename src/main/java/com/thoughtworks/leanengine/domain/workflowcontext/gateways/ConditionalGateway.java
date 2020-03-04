f93d2a70013e89c6abc4df3fd91b91633022f075
idolice <349031587@qq.com>
2020-03-04 15:51:55 +0800

    [Feat]: refactor conditionalgateway execute

--- src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/gateways/ConditionalGateway.java
+++ src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/gateways/ConditionalGateway.java
@@ -1,11 +1,15 @@
 package com.thoughtworks.leanengine.domain.workflowcontext.gateways;
 
+import static com.google.common.collect.Maps.newHashMap;
+
 import com.thoughtworks.leanengine.domain.workflowcontext.data.ComponentData;
 import com.thoughtworks.leanengine.domain.workflowcontext.data.WorkflowInstanceContext;
 import com.thoughtworks.leanengine.domain.workflowcontext.enums.ComponentType;
 import com.thoughtworks.leanengine.domain.workflowcontext.enums.Status;
 import com.thoughtworks.leanengine.domain.workflowcontext.interfaces.Component;
 import com.thoughtworks.leanengine.domain.workflowcontext.interfaces.Gateway;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 public class ConditionalGateway extends Gateway {
   public ConditionalGateway() {
@@ -13,12 +17,20 @@ public class ConditionalGateway extends Gateway {
   }
 
   private String conditionDescription;
-  private Boolean symbol;
+  private Boolean goFirstFlow;
 
   @Override
   public ComponentData execute(WorkflowInstanceContext workflowInstanceContext) {
+    LocalDateTime localDateTime = LocalDateTime.now();
+    Map<String, Object> data = newHashMap();
+    if (goFirstFlow) {
+      data.put("nextFlowId", this.getFirstFlowId());
+    } else {
+      data.put("nextFlowId", this.getSecondFlowId());
+    }
     this.setStatus(Status.SUCCESS);
-    return null;
+
+    return new ComponentData(this.getId(), localDateTime, LocalDateTime.now(), true, data);
   }
 
   @Override
--- src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/interfaces/Gateway.java
+++ src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/interfaces/Gateway.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.leanengine.domain.workflowcontext.interfaces;
 
 import com.thoughtworks.leanengine.domain.workflowcontext.enums.ComponentType;
+import lombok.Getter;
 
+@Getter
 public abstract class Gateway extends Component {
   private String firstFlowId;
   private String secondFlowId;
