5890a690be7c71404cb68f49571c6f422796c624
Huangyicai <yicai.huang@thoughtworks.com>
2020-03-05 15:30:49 +0800

    [Feat]: add api to runInstance.

---
+++ src/main/java/com/thoughtworks/leanengine/application/WorkflowInstanceUserCase.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.leanengine.application;
+
+import com.thoughtworks.leanengine.domain.workflowcontext.instance.WorkflowInstanceService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WorkflowInstanceUserCase {
+  private final WorkflowInstanceService workflowInstanceService;
+
+  public WorkflowInstanceUserCase(WorkflowInstanceService workflowInstanceService) {
+    this.workflowInstanceService = workflowInstanceService;
+  }
+
+  public boolean runInstance(String workflowId) {
+    return workflowInstanceService.runInstance(workflowId);
+  }
+}
--- src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/instance/WorkflowInstanceService.java
+++ src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/instance/WorkflowInstanceService.java
@@ -1,17 +1,24 @@
 package com.thoughtworks.leanengine.domain.workflowcontext.instance;
 
 import com.thoughtworks.leanengine.domain.workflowcontext.containers.Workflow;
+import com.thoughtworks.leanengine.infra.common.exceptions.WorkflowNotFoundException;
 import com.thoughtworks.leanengine.infra.repo.po.workflow.WorkflowInstanceContextPO;
+import com.thoughtworks.leanengine.infra.repo.po.workflow.WorkflowPO;
 import com.thoughtworks.leanengine.infra.repo.workflow.WorkflowInstanceContextRepository;
+import com.thoughtworks.leanengine.infra.repo.workflow.WorkflowRepository;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WorkflowInstanceService {
   private final WorkflowInstanceContextRepository workflowInstanceContextRepository;
+  private final WorkflowRepository workflowRepository;
 
   public WorkflowInstanceService(
-      WorkflowInstanceContextRepository workflowInstanceContextRepository) {
+      WorkflowInstanceContextRepository workflowInstanceContextRepository,
+      WorkflowRepository workflowRepository) {
     this.workflowInstanceContextRepository = workflowInstanceContextRepository;
+    this.workflowRepository = workflowRepository;
   }
 
   public void runInstance(Workflow workflow) {
@@ -20,5 +27,15 @@ public class WorkflowInstanceService {
     workflowInstanceContextRepository.save(
         WorkflowInstanceContextPO.of(
             workflow.getId(), workflowInstance.getWorkflowInstanceContext()));
+  }
+
+  public boolean runInstance(String workflowId) {
+    Optional<WorkflowPO> optionalWorkflowPO = workflowRepository.findById(workflowId);
+    if (!optionalWorkflowPO.isPresent()) {
+      throw new WorkflowNotFoundException();
+    }
+    Workflow workflow = optionalWorkflowPO.get().toDomainModel();
+    runInstance(workflow);
+    return true;
   }
 }
