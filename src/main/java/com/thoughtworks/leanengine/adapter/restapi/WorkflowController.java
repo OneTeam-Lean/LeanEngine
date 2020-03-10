aed3b21cabf2c71cbafa1d48d52b8bcd93794295
idolice <349031587@qq.com>
2020-03-10 11:09:34 +0800

    [Feat]: enable cors

--- src/main/java/com/thoughtworks/leanengine/adapter/restapi/WorkflowController.java
+++ src/main/java/com/thoughtworks/leanengine/adapter/restapi/WorkflowController.java
@@ -4,6 +4,7 @@ import com.thoughtworks.leanengine.adapter.restapi.dto.WorkflowCreateRequestDTO;
 import com.thoughtworks.leanengine.adapter.restapi.dto.WorkflowUpdateRequestDTO;
 import com.thoughtworks.leanengine.application.WorkflowUserCase;
 import com.thoughtworks.leanengine.domain.workflowcontext.workflow.Workflow;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/workflow")
+@CrossOrigin
 public class WorkflowController {
   private final WorkflowUserCase workflowUserCase;
 
