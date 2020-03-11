bbadaba1c217e5d1ea9d1cb6efb67591bb9628c8
Huangyicai <yicai.huang@thoughtworks.com>
2020-03-12 00:05:50 +0800

    [Fix]: fix conditionalGateway.

--- src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/components/gateways/ConditionalGateway.java
+++ src/main/java/com/thoughtworks/leanengine/domain/workflowcontext/components/gateways/ConditionalGateway.java
@@ -9,6 +9,7 @@ import com.thoughtworks.leanengine.domain.workflowcontext.workflow.ComponentExec
 import com.thoughtworks.leanengine.domain.workflowcontext.workflow.WorkflowExecution;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class ConditionalGateway extends Gateway {
 
@@ -20,7 +21,7 @@ public class ConditionalGateway extends Gateway {
 
   private String fromComponentId;
 
-  private int nextComponentIdx;
+  private int nextComponentIdx = 0;
 
   public ConditionalGateway(String fromComponentId) {
     super(ComponentType.CONDITIONAL_GATEWAY);
@@ -30,9 +31,8 @@ public class ConditionalGateway extends Gateway {
   @Override
   public ComponentExecutionData executeComponent(WorkflowExecution workflowExecution) {
     Map<String, Object> data = newHashMap();
-    Integer result =
-        (Integer) workflowExecution.getComponentExecutionData(fromComponentId, "randomNumber");
-    if (result == null || result > 50) {
+    int result = new Random().nextInt(100);
+    if (result > 50) {
       data.put(GATEWAY_FLAG, false);
       this.nextComponentIdx = 0;
     } else {
