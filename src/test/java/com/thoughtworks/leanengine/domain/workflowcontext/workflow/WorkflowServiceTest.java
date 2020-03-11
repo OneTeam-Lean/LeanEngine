413430022f410f5dce7b7d1de404ecbd2e0c4234
Huangyicai <yicai.huang@thoughtworks.com>
2020-03-11 23:08:36 +0800

    [Fix]: delete wrong test.

--- src/test/java/com/thoughtworks/leanengine/domain/workflowcontext/workflow/WorkflowServiceTest.java
+++ src/test/java/com/thoughtworks/leanengine/domain/workflowcontext/workflow/WorkflowServiceTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import com.thoughtworks.leanengine.ApiTestBase;
 import com.thoughtworks.leanengine.infra.repo.po.workflow.WorkflowPO;
 import com.thoughtworks.leanengine.infra.repo.workflow.WorkflowRepository;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -24,27 +23,6 @@ class WorkflowServiceTest extends ApiTestBase {
     assertEquals(
         workflowPO.getComponents().size(), workflowExecution.getComponentExecutions().size());
     WorkflowPO repoPO = workflowRepository.findByName("runTest");
-    assertNotNull(repoPO);
-    assertEquals(
-        repoPO.toDomainModel().getWorkflowExecutions().get(0).getComponentExecutions().size(),
-        workflowPO.getComponents().size());
-  }
-
-  @Ignore
-  @Test
-  void return_run_Component_when_runWorkflow() throws InterruptedException {
-    WorkflowPO workflowPO = workflowRepository.save(buildWorkflowPO("runStepTest"));
-
-    workflowService.runWorkflowByAsync(workflowPO.getId());
-    Thread.sleep(10000L);
-    WorkflowPO runTest = workflowRepository.findByName("runStepTest");
-    WorkflowExecution workflowExecution = runTest.getWorkflowExecutionPOs().get(0).toDomainModel();
-
-    assertNotNull(workflowExecution);
-
-    assertEquals(
-        workflowPO.getComponents().size(), workflowExecution.getComponentExecutions().size());
-    WorkflowPO repoPO = workflowRepository.findByName("runStepTest");
     assertNotNull(repoPO);
     assertEquals(
         repoPO.toDomainModel().getWorkflowExecutions().get(0).getComponentExecutions().size(),
